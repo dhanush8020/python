300696f14cfff6456529c27737a230e569183c14
Dhanush.B <94157320+dhanush8020@users.noreply.github.com>
2023-09-06 21:33:19 +0530

    second largest number

---
+++ 5.9.23.py
@@ -0,0 +1,11 @@
+# Sample list of numbers
+numbers = [10, 20, 5, 15, 30, 25]
+
+# Sort the list in descending order
+sorted_numbers = sorted(numbers, reverse=True)
+
+# The second-largest number is the element at index 1 (0-based indexing)
+second_largest = sorted_numbers[1]
+
+# Print the second-largest number
+print("The second-largest number is:", second_largest)
